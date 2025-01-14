f3709406f3a66117a8abad50fe9b6d141c92d428
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2025-01-14 17:33:42 +0530

    cleanup

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,98 +1,98 @@
-import { Request, Response as ExpressResponse } from "express";
-import User from "../models/User";
-import { AppError, Response } from "../core";
-import { generateAuthToken, toUserDTO } from "../utils";
-import { OAuth2Client } from "google-auth-library";
+// import { Request, Response as ExpressResponse } from "express";
+// import User from "../models/User";
+// import { AppError, Response } from "../core";
+// import { generateAuthToken, toUserDTO } from "../utils";
+// import { OAuth2Client } from "google-auth-library";
 
-const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+// const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-interface LoginRequest extends Request {
-  body: {
-    email: string;
-    password: string;
-  };
-}
+// interface LoginRequest extends Request {
+//   body: {
+//     email: string;
+//     password: string;
+//   };
+// }
 
-interface SignupRequest extends Request {
-  body: {
-    email: string;
-    password: string;
-    fullName: string;
-    grade: number;
-  };
-}
+// interface SignupRequest extends Request {
+//   body: {
+//     email: string;
+//     password: string;
+//     fullName: string;
+//     grade: number;
+//   };
+// }
 
-interface GoogleAuthRequest extends Request {
-  body: {
-    credential: string;
-  };
-}
+// interface GoogleAuthRequest extends Request {
+//   body: {
+//     credential: string;
+//   };
+// }
 
-export default class AuthController {
-  static async login(
-    req: LoginRequest,
-    res: ExpressResponse
-  ): Promise<ExpressResponse> {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+// export default class AuthController {
+//   static async login(
+//     req: LoginRequest,
+//     res: ExpressResponse
+//   ): Promise<ExpressResponse> {
+//     const { email, password } = req.body;
+//     const user = await User.findOne({ email });
 
-    if (!user || !(await user.comparePassword(password))) {
-      throw AppError.unauthorized("Invalid credentials");
-    }
+//     if (!user || !(await user.comparePassword(password))) {
+//       throw AppError.unauthorized("Invalid credentials");
+//     }
 
-    const token = generateAuthToken(user);
-    return res.json(
-      Response.success({ token, user: toUserDTO(user.toObject()) })
-    );
-  }
+//     const token = generateAuthToken(user);
+//     return res.json(
+//       Response.success({ token, user: toUserDTO(user.toObject()) })
+//     );
+//   }
 
-  static async signup(
-    req: SignupRequest,
-    res: ExpressResponse
-  ): Promise<ExpressResponse> {
-    const { email, password, fullName, grade } = req.body;
+//   static async signup(
+//     req: SignupRequest,
+//     res: ExpressResponse
+//   ): Promise<ExpressResponse> {
+//     const { email, password, fullName, grade } = req.body;
 
-    if (await User.findOne({ email })) {
-      throw AppError.badRequest("Email already exists");
-    }
+//     if (await User.findOne({ email })) {
+//       throw AppError.badRequest("Email already exists");
+//     }
 
-    const user = await User.create({ email, password, fullName, grade });
-    const token = generateAuthToken(user);
+//     const user = await User.create({ email, password, fullName, grade });
+//     const token = generateAuthToken(user);
 
-    return res.json(
-      Response.success({ token, user: toUserDTO(user.toObject()) })
-    );
-  }
+//     return res.json(
+//       Response.success({ token, user: toUserDTO(user.toObject()) })
+//     );
+//   }
 
-  static async googleAuth(
-    req: GoogleAuthRequest,
-    res: ExpressResponse
-  ): Promise<ExpressResponse> {
-    const { credential } = req.body;
-    const ticket = await googleClient.verifyIdToken({
-      idToken: credential,
-      audience: process.env.GOOGLE_CLIENT_ID,
-    });
+//   static async googleAuth(
+//     req: GoogleAuthRequest,
+//     res: ExpressResponse
+//   ): Promise<ExpressResponse> {
+//     const { credential } = req.body;
+//     const ticket = await googleClient.verifyIdToken({
+//       idToken: credential,
+//       audience: process.env.GOOGLE_CLIENT_ID,
+//     });
 
-    const payload = ticket.getPayload();
-    if (!payload) {
-      throw AppError.badRequest("Failed to sign in with google!");
-    }
+//     const payload = ticket.getPayload();
+//     if (!payload) {
+//       throw AppError.badRequest("Failed to sign in with google!");
+//     }
 
-    const { email, name: fullName, sub: googleId } = payload;
+//     const { email, name: fullName, sub: googleId } = payload;
 
-    let user = await User.findOne({ googleId });
-    if (!user) {
-      user = await User.create({
-        email,
-        fullName,
-        googleId,
-      });
-    }
-    const token = generateAuthToken(user);
+//     let user = await User.findOne({ googleId });
+//     if (!user) {
+//       user = await User.create({
+//         email,
+//         fullName,
+//         googleId,
+//       });
+//     }
+//     const token = generateAuthToken(user);
 
-    return res.json(
-      Response.success({ token, user: toUserDTO(user.toObject()) })
-    );
-  }
-}
+//     return res.json(
+//       Response.success({ token, user: toUserDTO(user.toObject()) })
+//     );
+//   }
+// }
--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,42 +1,41 @@
-import jwt, { JwtPayload } from 'jsonwebtoken';
-import { Request, Response, NextFunction } from 'express';
-import User from '../models/User.js';
-import { AppError } from '../core';
+// import jwt, { JwtPayload } from 'jsonwebtoken';
+// import { Request, Response, NextFunction } from 'express';
+// import { AppError } from '../core';
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: DecodedToken;
-    }
-  }
-}
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       user?: DecodedToken;
+//     }
+//   }
+// }
 
-interface DecodedToken extends JwtPayload {
-  id: string;
-  grade: number;
-}
+// interface DecodedToken extends JwtPayload {
+//   id: string;
+//   grade: number;
+// }
 
-export default async function auth(req: Request, res: Response, next: NextFunction): Promise<void> {
-  const authHeader = req.headers.authorization;
+// export default async function auth(req: Request, res: Response, next: NextFunction): Promise<void> {
+//   const authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    throw AppError.unauthorized('No token provided');
-  }
+//   if (!authHeader) {
+//     throw AppError.unauthorized('No token provided');
+//   }
 
-  const token = authHeader.split(' ')[1];
+//   const token = authHeader.split(' ')[1];
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as DecodedToken;
-    // console.log('decoded - ', decoded)
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as DecodedToken;
+//     // console.log('decoded - ', decoded)
 
-    // TODO: assign required user info in jwt payload itself when token is generated
-    req.user = decoded;
+//     // TODO: assign required user info in jwt payload itself when token is generated
+//     req.user = decoded;
 
-    next();
-  } catch (error) {
-    if (error instanceof jwt.TokenExpiredError) {
-      throw AppError.unauthorized('Token expired');
-    }
-    throw AppError.unauthorized('Invalid token');
-  }
-}
+//     next();
+//   } catch (error) {
+//     if (error instanceof jwt.TokenExpiredError) {
+//       throw AppError.unauthorized('Token expired');
+//     }
+//     throw AppError.unauthorized('Invalid token');
+//   }
+// }
